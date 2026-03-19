2dc6cfc19a51a5b2d399f3c5716673a378a8fa2a
Sergio Machado <sergio@umbc.edu>
2026-03-18 23:23:19 -0400

    SSM: added GDEMM2024 to DEM_DigitalELeveationModel as it seems better behaved than ETOPO2022; see COMMON/set_landfrac_using_L1B_L1C_or_usgs.m

--- CLUST_RTPMAKE/COMMON_SETTINGS/set_landfrac_using_L1B_L1C_or_usgs.m
+++ CLUST_RTPMAKE/COMMON_SETTINGS/set_landfrac_using_L1B_L1C_or_usgs.m
@@ -1,5 +1,6 @@
 iUSGS = +1;  %% swtich using usgs      but we lost the database!
 iUSGS = +2;  %% swtich using ETOPO2022 landfrac is good but salti for lakes and eg bahamas, S. Pacific is bad
+iUSGS = +3;  %% swtich using GDEMM2024 landfrac is good and salti seems good
 iUSGS = -1;  %% stick with L1B/L1C
 
 if iUSGS == 1
@@ -13,6 +14,13 @@ elseif iUSGS == 2
   p.landfrac_fromL1B = p.landfrac;
   p.salti_fromL1B = p.salti;
   [salti,landfrac,gebco] = etopo_dem_and_imerg_lf(p.rlat,p.rlon);
+  p.landfrac = landfrac;
+  p.salti    = salti;
+elseif iUSGS == 3
+  addpath /home/sergio/git/matlabcode/DEM_DigitalELeveationModel
+  p.landfrac_fromL1B = p.landfrac;
+  p.salti_fromL1B = p.salti;
+  [salti,landfrac,gebco] = gdemm_dem_and_imerg_lf(p.rlat,p.rlon);
   p.landfrac = landfrac;
   p.salti    = salti;
 else
